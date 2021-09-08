df91df434103d52517d14ac67e6fb957757b6575
monaalbood <hassanalbood123@gmail.com>
2021-09-08 03:22:30 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# programming-courses-v4pjpb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/programming-courses-v4pjpb)
---
+++ index.ts
@@ -0,0 +1,16 @@
+//######################################################################
+//###########################           SSTech       ###################
+//###########################   Programming Courses  ###################
+//######################################################################
+lot myNumber1,myNumber2 :number;
+myNumber1 =151;
+myNumber2 =256;
+
+consol.log("محتوى المتغيرة الاولى ",myNumber1);
+consol.log ("محتوى المتغيرة الثانية " .mynumber2);
+lot myNumber1,myNumber2:number;
+lot myNumber3:unmber
+myNumber1=151;
+myNumber256;
+console.log(محتوى المتغيرةالاولىوه,myNumber1);
+console.log ("محتوى المتغيرة الثانية ",myNumber2);
